529b99499f394a7242f80cf6049498080624bf7f
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 12:14:28 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    
+        public static void main(String[] args ){
+            String str ="hello, Manikanta";
+            String a ="welcome to java tutorial";
+            System.out.println(str);
+            System.out.println(str.toUpperCase());
+            System.out.println(str.toLowerCase());
+            System.out.println(str.charAt(3));
+            System.out.println(str.indexOf('h'));
+            System.out.println(str.length());
+        }
+    }
+
